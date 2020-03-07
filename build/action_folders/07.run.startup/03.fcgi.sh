adbf9f34bc4be1524ec1f3ccb678b2805397d5ee
Ballantyne, Bob <bob.ballantyne@emc.com>
2020-03-06 19:10:12 -0500

    rename fcgi.redirect to fcgiwrap.redirect to be consistent

--- build/action_folders/07.run.startup/03.fcgi.sh
+++ build/action_folders/07.run.startup/03.fcgi.sh
@@ -3,6 +3,6 @@
 : ${WWW_UID:?"Environment variable 'WWW_UID' not defined in '${BASH_SOURCE[0]}'"}
 
 sed -i "s|^socket=.*$|socket=unix://${RUN_DIR}/fcgiwrap.sock|"         /etc/supervisor.d/fcgiwrap.ini
-sed -i "s|^\s*server\s+.*$|    server unix:${RUN_DIR}/fcgiwrap.sock;|" /etc/nginx/conf.d/fcgi.upstream
+sed -i "s|^\s*server\s+.*$|    server unix:${RUN_DIR}/fcgiwrap.sock;|" /etc/nginx/conf.d/fcgiwrap.upstream
 
 crf.fixupDirectory "$RUN_DIR" "$WWW_UID"
