26dc615b655f5b58107ec8fea0603215534a238e
Ballantyne, Bob <bob.ballantyne@emc.com>
2020-03-27 19:52:17 -0400

    add logging based on PHP_LOGGING environment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # php7
+
+runtime environment variable PHP_LOGGING=1 enables logging
--- build/action_folders/07.run.startup/04.php.sh
+++ build/action_folders/07.run.startup/04.php.sh
@@ -3,19 +3,19 @@
 : ${WWW_UID:?"Environment variable 'WWW_UID' not defined in '${BASH_SOURCE[0]}'"}
 
 declare iniFile="/etc/${PHP}/php.ini"
-term.log "    updating '$iniFile' with 'open_basedir = ${WWW}'\n" 'white' 
+term.log "    updating '$iniFile' with 'open_basedir = ${WWW}'\n" 'white'
 sed -i -e "s|^.*date.timezone\s*=.*$|date.timezone = ${TZ}|" \
        -e "s|^.*session.save_path\s*=.*$|session.save_path = \"${SESSIONS_DIR}\"|" \
        -e "s|^.*open_basedir\s*=.*$|open_basedir = ${WWW}/:/tmp/:/etc/phpmyadmin/|" "$iniFile"
 
 
 declare cnfFile="/etc/${PHP}/php-fpm.conf"
-term.log "    updating '$cnfFile' with 'chdir = ${WWW}'\n" 'white' 
+term.log "    updating '$cnfFile' with 'chdir = ${WWW}'\n" 'white'
 sed -i -e "s|^listen\s*=.*$|listen=${RUN_DIR}/php-fpm.sock|"  \
        -e "s|^.*user\s*=.*$|user = ${WWW_USER}|" \
        -e "s|^.*group\s*=.*$|group = ${WWW_GROUP}|" \
        -e "s|^.*chdir\s*=.*$|chdir = ${WWW}|" \
-          "$cnfFile" 
+          "$cnfFile"
 
 sed -i "s|^\s*server\s+.*$|    server unix:${RUN_DIR}/php-fpm.sock;|"  /etc/nginx/conf.d/php_fpm.upstream
 
@@ -35,4 +35,43 @@ if [ -f /var/log/.nginx.debug ] &&  [ -e "$cnfFile" ] ; then
     sed -Ei \
         -e "s|^.*zend_extension=xdebug.so$|zend_extension=xdebug.so|" \
            "$__file"
-fi 
+fi
+
+if [ "${PHP_LOGGING:-}" ];then
+    declare cfgFile='/etc/php7/php.ini'
+    term.log "    updating '${cfgFile}' with provided 'LOG_LEVEL'"'\n' 'white'
+
+    if [ "$PHP_LOGGING" = 0 ]; then
+        sed -i -E -e "s|^(error_reporting\s*=\s*).*$|\E_ALL & ~E_DEPRECATED & ~E_STRICT|g" \
+                  -e "s|^(display_errors\s*=\s*).*$|\1Off|g" \
+                  -e "s|^(display_startup_errors\s*=\s*).*$|\1Off|g" \
+                  -e "s|^(log_errors\s*=\s*).*$|\1Off|g" \
+                  -e "s|^(track_errors\s*=\s*).*$|\1Off|g" \
+                  -e "s|^(html_errors\s*=\s*).*$|\1Off|g" \
+                     "$cfgFile"
+    else
+        sed -i -E -e "s|^(error_reporting\s*=\s*).*$|\1E_ALL|g" \
+                  -e "s|^(display_errors\s*=\s*).*$|\1On|g" \
+                  -e "s|^(display_startup_errors\s*=\s*).*$|\1On|g" \
+                  -e "s|^(log_errors\s*=\s*).*$|\1On|g" \
+                  -e "s|^(track_errors\s*=\s*).*$|\1On|g" \
+                  -e "s|^(html_errors\s*=\s*).*$|\1On|g" \
+                     "$cfgFile"
+
+    fi
+
+    cfgFile='/etc/php5/php-fpm.conf'
+    term.log "    updating '${cfgFile}' with provided 'LOG_LEVEL'"'\n' 'white'
+
+    if [ "$PHP_LOGGING" = 0 ]; then
+        sed -i -E -e "s|^(log_level\s*=\s*).*$|\1warning|" \
+                  -e "s|^;?(access.log.*)$|;\1|" \
+                     "$cfgFile"
+    else
+        sed -i -E -e "s|^(log_level\s*=\s*).*$|\1debug|" \
+                  -e "s|^;?(access.log\s*=\s*).*$|\1/var/log/php-fpm.access.log|" \
+                     "$cfgFile"
+        touch /var/log/php-fpm.access.log
+    fi
+fi
+
